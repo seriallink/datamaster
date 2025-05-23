9a9390019ab22a23e69a4fede457a92635143d01
Monaco <marcelo.monaco@gmail.com>
2025-05-23 18:15:45 -0300

    Minor adjustments in migration

--- cli/cmd/migration.go
+++ cli/cmd/migration.go
@@ -4,7 +4,6 @@ import (
 	"embed"
 	"flag"
 	"fmt"
-	"io"
 	"strings"
 
 	"github.com/seriallink/datamaster/cli/core"
@@ -18,13 +17,10 @@ func MigrationCmd(migrations embed.FS) *ishell.Cmd {
 		Name: "migration",
 		Help: "Run database migration scripts",
 		Func: WithAuth(func(c *ishell.Context) {
+
 			fs := flag.NewFlagSet("migration", flag.ContinueOnError)
-			fs.SetOutput(io.Discard)
-
 			script := fs.String("script", "", "Optional script name to run (e.g., xyz.sql)")
-
-			if err := fs.Parse(c.Args); err != nil {
-				c.Println(misc.Red(fmt.Sprintf("Invalid arguments: %v", err)))
+			if !ParseShellFlags(c, fs) {
 				return
 			}
 
