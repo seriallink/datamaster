445d9f52c71b07b334d117f8cea81527e3a95713
Monaco <marcelo.monaco@gmail.com>
2025-06-20 12:40:49 -0300

    Replicate bronze tables to silver with date enrichment

--- database/migrations/002_create_dm_view.sql
+++ database/migrations/002_create_dm_view.sql
@@ -1,6 +1,6 @@
 ﻿/*
 Created: 5/6/2025
-Modified: 6/10/2025
+Modified: 6/13/2025
 Project: Data Master
 Model: DataMaster
 Author: Marcelo Monaco
@@ -9,70 +9,105 @@ Database: Aurora PostgreSQL 9.5
 
 -- Create schemas section -------------------------------------------------
 
-CREATE SCHEMA dm_core
+CREATE SCHEMA dm_view
 ;
-COMMENT ON SCHEMA dm_core IS 'Raw transactional data schema. Mirrors the source systems without transformation. Used to populate the Bronze layer in the data lake.'
+COMMENT ON SCHEMA dm_view IS 'Normalized and enriched views derived from core tables. Represents cleaned and deduplicated data used to populate the Silver layer in the data lake.'
 ;
 
 -- Create tables section -------------------------------------------------
 
--- Table dm_core.brewery
+-- Table dm_view.brewery
 
-CREATE TABLE dm_core.brewery
+CREATE TABLE dm_view.brewery
 (
   brewery_id Bigint NOT NULL,
-  brewery_name Text NOT NULL
+  brewery_name Text NOT NULL,
+  created_at Timestamptz NOT NULL,
+  updated_at Timestamptz NOT NULL,
+  deleted_at Timestamptz NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE dm_core.brewery ADD CONSTRAINT pk_brewery PRIMARY KEY (brewery_id)
+ALTER TABLE dm_view.brewery ADD CONSTRAINT pk_brewery PRIMARY KEY (brewery_id)
 ;
 
--- Table dm_core.beer
+-- Table dm_view.beer
 
-CREATE TABLE dm_core.beer
+CREATE TABLE dm_view.beer
 (
   beer_id Bigint NOT NULL,
-  brewery_id Bigint NOT NULL,
   beer_name Text NOT NULL,
   beer_style Text NOT NULL,
-  beer_abv Numeric
+  beer_abv Numeric,
+  created_at Timestamptz NOT NULL,
+  updated_at Timestamptz NOT NULL,
+  deleted_at Timestamptz NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-CREATE INDEX ix_beer_brewery ON dm_core.beer (brewery_id)
+ALTER TABLE dm_view.beer ADD CONSTRAINT pk_beer PRIMARY KEY (beer_id)
 ;
 
-ALTER TABLE dm_core.beer ADD CONSTRAINT pk_beer PRIMARY KEY (beer_id)
-;
+-- Table dm_view.profile
 
--- Table dm_core.profile
-
-CREATE TABLE dm_core.profile
+CREATE TABLE dm_view.profile
 (
   profile_id Bigint NOT NULL,
   profile_name Text NOT NULL,
   email Text NOT NULL,
-  state Text NOT NULL
+  state Text NOT NULL,
+  created_at Timestamptz NOT NULL,
+  updated_at Timestamptz NOT NULL,
+  deleted_at Timestamptz NOT NULL
 )
 WITH (
   autovacuum_enabled=true)
 ;
 
-ALTER TABLE dm_core.profile ADD CONSTRAINT pk_profile PRIMARY KEY (profile_id)
+ALTER TABLE dm_view.profile ADD CONSTRAINT pk_profile PRIMARY KEY (profile_id)
 ;
 
--- Table dm_core.review
+-- Table dm_view.review
 
-CREATE TABLE dm_core.review
+CREATE TABLE dm_view.review
 (
   review_id Bigint NOT NULL,
+  review_overall Numeric,
+  review_aroma Numeric,
+  review_appearance Numeric,
+  review_palate Numeric,
+  review_taste Numeric,
+  review_time Timestamptz NOT NULL,
+  created_at Timestamptz NOT NULL,
+  updated_at Timestamptz NOT NULL,
+  deleted_at Timestamptz NOT NULL
+)
+WITH (
+  autovacuum_enabled=true)
+;
+
+ALTER TABLE dm_view.review ADD CONSTRAINT PK_review PRIMARY KEY (review_id)
+;
+
+-- Table dm_view.review_flat
+
+CREATE TABLE dm_view.review_flat
+(
+  review_id Bigint NOT NULL,
+  brewery_id Bigint NOT NULL,
   beer_id Bigint NOT NULL,
   profile_id Bigint NOT NULL,
+  brewery_name Text,
+  beer_name Text,
+  beer_style Text,
+  beer_abv Numeric,
+  profile_name Text,
+  email Text,
+  state Text,
   review_overall Numeric,
   review_aroma Numeric,
   review_appearance Numeric,
@@ -84,38 +119,6 @@ WITH (
   autovacuum_enabled=true)
 ;
 
-CREATE INDEX ix_review_beer ON dm_core.review (beer_id)
-;
-
-CREATE INDEX ix_review_profile ON dm_core.review (profile_id)
-;
-
-ALTER TABLE dm_core.review ADD CONSTRAINT PK_review PRIMARY KEY (review_id)
-;
-
--- Create foreign keys (relationships) section -------------------------------------------------
-
-ALTER TABLE dm_core.beer
-  ADD CONSTRAINT fk_beer_brewery
-    FOREIGN KEY (brewery_id)
-    REFERENCES dm_core.brewery (brewery_id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-;
-
-ALTER TABLE dm_core.review
-  ADD CONSTRAINT fk_review_beer
-    FOREIGN KEY (beer_id)
-    REFERENCES dm_core.beer (beer_id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-;
-
-ALTER TABLE dm_core.review
-  ADD CONSTRAINT fk_review_profile
-    FOREIGN KEY (profile_id)
-    REFERENCES dm_core.profile (profile_id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
+ALTER TABLE dm_view.review_flat ADD CONSTRAINT pk_review_flat PRIMARY KEY (review_id)
 ;
 
